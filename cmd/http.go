c7328b93a38373d1b1119bdf028d1289a1662115
Hangyuan Zou <99110203+yuuuuuuan@users.noreply.github.com>
2025-01-07 01:59:07 +0000

    add

--- cmd/http.go
+++ cmd/http.go
@@ -1,5 +1,23 @@
 package cmd
 
-import(
-	
+import (
+	"fmt"
+	"oftools/algorithm"
+
+	"github.com/urfave/cli/v2"
 )
+
+// GreetCommand is a simple command to greet the user.
+var HttpCommand = &cli.Command{
+	Name:    "http",
+	Aliases: []string{"ht"},
+	Usage:   "Greets the user with the provided name",
+	Action: func(c *cli.Context) error {
+		//var err error
+		err := algorithm.Http()
+		if err != nil {
+			return fmt.Errorf("invalid decimal value: %v", err)
+		}
+		return nil
+	},
+}
--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 			cmd.ExcelCommand,
 			cmd.JumpCommand,
 			cmd.ProgramCommand,
+			cmd.HttpCommand,
 		},
 	}
 
