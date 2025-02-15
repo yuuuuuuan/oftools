7fac3f6cbcbb6f866fac127c28f4c42b5e1356db
Hangyuan Zou <99110203+yuuuuuuan@users.noreply.github.com>
2025-02-15 06:42:55 +0000

    add

--- DownloadTools/interface.go
+++ DownloadTools/interface.go
@@ -1,50 +1,24 @@
 package main
 
 import (
-	"os"
-
-	"github.com/therecipe/qt/widgets"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
-//baseurl := "http://192.168.124.126/client"
-
-type Interface struct {
-	Yq []int
-	Lx int
-	Cs map[string]string
-}
-
 func main() {
-	// 1. 初始化QApplication
-	app := widgets.NewQApplication(len(os.Args), os.Args)
-	// 2. 创建主窗口
-	window := widgets.NewQMainWindow(nil, 0)
-	window.SetWindowTitle("Go + Qt桌面客户端示例")
-	window.Resize2(400, 200)
+	a := app.New()
+	w := a.NewWindow("Hello")
 
-	// 3. 创建中央部件和布局
-	centralWidget := widgets.NewQWidget(nil, 0)
-	layout := widgets.NewQVBoxLayout2(centralWidget)
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
 
-	// 创建QComboBox
-	comboBox := widgets.NewQComboBox(nil)
-	//comboBox.SetPlaceholderText("请选择一个选项")
-	comboBox.AddItem("选项1", nil)
-	comboBox.AddItem("选项2", nil)
-	comboBox.AddItem("选项3", nil)
-
-	// 设置默认选项为“选项1”
-	comboBox.SetCurrentIndex(0)
-
-	// 将QComboBox添加到布局中
-	layout.AddWidget(comboBox, 0, 0)
-
-	// 设置中心部件并显示窗口
-	window.SetCentralWidget(centralWidget)
-	window.Show()
-
-	// 4. 执行应用
-	app.Exec()
+	w.ShowAndRun()
 }
 
 //CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-H windowsgui -s -w" -o DownloadTools.exe interface.go
