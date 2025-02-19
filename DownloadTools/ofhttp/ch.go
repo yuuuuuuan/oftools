2a395b156c8eae9003688055c2992e00dc14e6a4
Hangyuan Zou <99110203+yuuuuuuan@users.noreply.github.com>
2025-02-19 06:25:44 +0000

    add

--- DownloadTools/ofhttp/ch.go
+++ DownloadTools/ofhttp/ch.go
@@ -6,10 +6,12 @@ func SendPostRequset(url string, reqChan chan map[string]interface{}, respChan c
 	for req := range reqChan {
 		resp, err := HttpPost(url, req)
 		if err != nil {
+			print("chan err1")
 			log.Fatal(err)
 		}
 		res, err := ConvertRespToJson(resp)
 		if err != nil {
+			print("chan err2")
 			log.Fatal(err)
 		}
 		respChan <- res
