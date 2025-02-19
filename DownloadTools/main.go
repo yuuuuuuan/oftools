5846c1f8ade6cc485a66a510f025743141d1f07d
Hangyuan Zou <99110203+yuuuuuuan@users.noreply.github.com>
2025-02-19 08:50:15 +0000

    add

--- DownloadTools/main.go
+++ DownloadTools/main.go
@@ -4,27 +4,30 @@ import (
 	"encoding/json"
 	"log"
 	"oftools/DownloadTools/ofhttp"
+	"os"
 	"time"
+
+	"github.com/therecipe/qt/widgets"
 )
 
 const baseurl = "http://192.168.124.126/client"
 
 func main() {
-	// // 创建应用程序
-	// app := widgets.NewQApplication(len(os.Args), os.Args)
+	// 创建应用程序
+	app := widgets.NewQApplication(len(os.Args), os.Args)
 
-	// // 创建主窗口
-	// window := widgets.NewQMainWindow(nil, 0)
-	// window.SetWindowTitle("V1.0.0.17")
-	// window.SetMinimumSize2(400, 300)
+	// 创建主窗口
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetWindowTitle("V1.0.0.17")
+	window.SetMinimumSize2(400, 300)
 
-	// // 创建中央部件
-	// centralWidget := widgets.NewQWidget(nil, 0)
-	// window.SetCentralWidget(centralWidget)
+	// 创建中央部件
+	centralWidget := widgets.NewQWidget(nil, 0)
+	window.SetCentralWidget(centralWidget)
 
-	// // 创建布局
-	// layout := widgets.NewQVBoxLayout()
-	// centralWidget.SetLayout(layout)
+	// 创建布局
+	layout := widgets.NewQVBoxLayout()
+	centralWidget.SetLayout(layout)
 
 	//var reqChan1, respChan1 chan map[string]interface{}
 	//var reqChan2, respChan2 chan map[string]interface{}
@@ -65,20 +68,20 @@ func main() {
 	}
 
 	log.Println(values)
-	// domainLabel := widgets.NewQLabel2("园区：", nil, 0)
-	// domainComboBox := widgets.NewQComboBox(nil)
-	// domainComboBox.AddItems(values)
+	domainLabel := widgets.NewQLabel2("园区：", nil, 0)
+	domainComboBox := widgets.NewQComboBox(nil)
+	domainComboBox.AddItems(values)
 
-	// domainLayout := widgets.NewQHBoxLayout()
-	// domainLayout.AddWidget(domainLabel, 0, 0)
-	// domainLayout.AddWidget(domainComboBox, 0, 0)
-	// layout.AddLayout(domainLayout, 0)
-	// println("2")
-	// // 显示窗口
-	// window.Show()
+	domainLayout := widgets.NewQHBoxLayout()
+	domainLayout.AddWidget(domainLabel, 0, 0)
+	domainLayout.AddWidget(domainComboBox, 0, 0)
+	layout.AddLayout(domainLayout, 0)
+	println("2")
+	// 显示窗口
+	window.Show()
 
-	// // 运行 Qt 主循环
-	// app.Exec()
+	// 运行 Qt 主循环
+	app.Exec()
 }
 
 //CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-H windowsgui -s -w" -o DownloadTools.exe main.go
